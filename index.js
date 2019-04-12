bc047f8bfe6e3ba2ed2b672429e32ec5f0ba5c31
Rajdeep Barman <deepBn9@gmail.com>
2019-04-12 19:17:00 +0530

    added login screen

--- index.js
+++ index.js
@@ -3,17 +3,17 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import React from 'react';
-import {AppRegistry} from 'react-native';
+//import React from 'react';
+//import {AppRegistry} from 'react-native';
 import App from './App';
-import {Provider} from 'react-redux';
-import {name as appName} from './app.json';
-import configureStore from './src/store/configureStore';
+//import {Provider} from 'react-redux';
+//import {name as appName} from './app.json';
+//import configureStore from './src/store/configureStore';
 
-const RNRedux = () => (
+/*const RNRedux = () => (
   <Provider store={configureStore}>
     <App/>
   </Provider>
-);
+);*/
 
-AppRegistry.registerComponent(appName, () => RNRedux);
+//AppRegistry.registerComponent(appName, () => App);
