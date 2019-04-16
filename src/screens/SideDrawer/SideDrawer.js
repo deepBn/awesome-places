788e3df07d7c5c841369f967de29a35abd17cfe9
Rajdeep Barman <deepBn9@gmail.com>
2019-04-16 19:07:29 +0530

    showing sidedrawer on all screens

--- src/screens/SideDrawer/SideDrawer.js
+++ src/screens/SideDrawer/SideDrawer.js
@@ -1,14 +1,22 @@
 import React, {Component} from 'react';
-import {View, Text, Dimensions} from 'react-native';
+import {View, Text, Dimensions, StyleSheet} from 'react-native';
 
 class SideDrawer extends Component {
   render() {
     return (
-      <View style={{width: Dimensions.get('window').width * 0.8}}>
+      <View style={[styles.container, {width: Dimensions.get('window').width * 0.8}]}>
         <Text>Sidedrawer</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    paddingTop: 22,
+    flex: 1
+  }
+});
 
 export default SideDrawer;
