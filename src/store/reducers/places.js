d925ebe7999fa4731bf3d03b71a7ff4b3a2d5a37
Rajdeep Barman <deepBn9@gmail.com>
2019-04-16 12:08:10 +0530

    added find place and share place screen components

--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -1,8 +1,7 @@
-import {ADD_PLACE, DELETE_PLACE, SELECT_PLACE, DESELECT_PLACE} from '../actions/actionTypes';
+import {ADD_PLACE, DELETE_PLACE} from '../actions/actionTypes';
 
 const initialState = {
-  places: [],
-  selectedPlace: null
+  places: []
 };
 
 const reducer = (state = initialState, action) => {
@@ -19,18 +18,7 @@ const reducer = (state = initialState, action) => {
     case DELETE_PLACE:
       return {
         ...state,
-        places: state.places.filter(place => place.id !== state.selectedPlace.id),
-        selectedPlace: null
-      };
-    case SELECT_PLACE:
-      return {
-        ...state,
-        selectedPlace: state.places.find(place => place.id === action.placeId)
-      };
-    case DESELECT_PLACE:
-      return {
-        ...state,
-        selectedPlace: null
+        places: state.places.filter(place => place.id !== state.selectedPlace.id)
       };
     default:
       return state;
