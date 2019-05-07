5e80015e9e8f3aebccde2e8af9508d70c0663a4e
Rajdeep Barman <deepBn9@gmail.com>
2019-05-07 14:42:22 +0530

    added side drawer item

--- src/screens/SideDrawer/SideDrawer.js
+++ src/screens/SideDrawer/SideDrawer.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
-import {View, Text, Dimensions, StyleSheet} from 'react-native';
+import {View, Text, Dimensions, StyleSheet, TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 class SideDrawer extends Component {
   render() {
     return (
       <View style={[styles.container, {width: Dimensions.get('window').width * 0.8}]}>
-        <Text>Sidedrawer</Text>
+        <TouchableOpacity>
+          <View style={styles.drawerItem}>
+            <Icon name="logout-variant" size={30} style={styles.drawerItemIcon}/>
+            <Text>Sidedrawer</Text>
+          </View>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -14,8 +20,18 @@ class SideDrawer extends Component {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: 'white',
-    paddingTop: 22,
+    paddingTop: 30,
     flex: 1
+  },
+  drawerItem: {
+    flexDirection:'row',
+    alignItems: 'center',
+    padding: 10,
+    backgroundColor: '#eee'
+  },
+  drawerItemIcon: {
+    color: '#aaa',
+    marginRight: 10
   }
 });
 
