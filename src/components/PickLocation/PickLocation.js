bed9c1694542ed6b567b8be8432d284499db264a
Rajdeep Barman <deepBn9@gmail.com>
2019-05-10 17:44:24 +0530

    added react native maps

--- src/components/PickLocation/PickLocation.js
+++ src/components/PickLocation/PickLocation.js
@@ -1,13 +1,39 @@
 import React, {Component} from 'react';
-import {Button, StyleSheet, Text, View} from "react-native";
+import {Button, StyleSheet, View, Dimensions} from "react-native";
+import MapView from 'react-native-maps';
 
 class PickLocation extends Component {
+  state = {
+    focusedLocation: {
+      latitude: 22.6757521,
+      longitude: 88.0495361,
+      latitudeDelta: 0.0122,
+      longitudeDelta: (Dimensions.get('window').width / Dimensions.get('window').height) * 0.0122
+    }
+  };
+
+  pickLocationHandler = event => {
+    const coords = event.nativeEvent.coordinate;
+    this.setState(prevState => {
+      return {
+        focusedLocation: {
+          ...prevState.focusedLocation,
+          latitude: coords.latitude,
+          longitude: coords.longitude
+        }
+      }
+    })
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.placeholder}>
-          <Text>Map</Text>
-        </View>
+        <MapView
+          style={styles.map}
+          region={this.state.focusedLocation}
+          initialRegion={this.state.focusedLocation}
+          onPress={this.pickLocationHandler}
+        />
         <View style={styles.button}>
           <Button title="Locate Me" onPress={() => undefined}/>
         </View>
@@ -21,12 +47,9 @@ const styles = StyleSheet.create({
     width: '100%',
     alignItems: 'center'
   },
-  placeholder: {
-    borderWidth: 1,
-    borderColor: 'black',
-    backgroundColor: '#eee',
-    width: '80%',
-    height: 150
+  map: {
+    width: '100%',
+    height: 250
   },
   button: {
     margin: 8
